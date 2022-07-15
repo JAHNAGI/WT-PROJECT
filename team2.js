4cf14ad659bb45316071cd725383be849bf4ffaa
Ashok5678 <108926394+Ashok5678@users.noreply.github.com>
2022-07-15 16:33:38 +0530

    Update team2.js

--- team2.js
+++ team2.js
@@ -1,16 +1,1 @@
-function check(){
-    var names=["school","school2","board"];
-    var ids=["jah1","jah2","jah3","jah4"];
-    var a=document.forms[0];
-   for (var i=0;i<names.length;i++){
-    if (a[names[i]].value==""){
-        document.getElementById(ids[i]).nextElementSibling.innerHTML="helo";
-        document.getElementById("ask").innerHTML+="this field is required<br>";
-        console.log("hello");
-        
-    }
-  
-    
-   }
-   return false;
-}
+
